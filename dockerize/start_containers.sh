95d307b208a6ce6f96c32c73c841e34edfc87067
anhminhbo <wormscott12397@gmail.com>
2022-10-30 11:14:02 +0700

    update proxy

--- dockerize/start_containers.sh
+++ dockerize/start_containers.sh
@@ -8,7 +8,7 @@ backendLocalUrl="http\:\/\/localhost\:8080"
 backendProdUrl="http\:\/\/20.189.121.242"
 
 # Gen for productiom
-cat docker-compose.yaml.production.template | \
+cat docker-compose.production.yaml.template | \
     sed "s/\[\[FRONTEND_TAG\]\]/$frontendTag/g" | \
     sed "s/\[\[PROXY_TAG\]\]/$proxyTag/g" | \
     sed "s/\[\[BACKEND_TAG\]\]/$backendTag/g" | \
