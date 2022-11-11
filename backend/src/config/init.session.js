6fb02fd0ad61f37ba4f03dff66ee34be05c883eb
anhminhbo <wormscott12397@gmail.com>
2022-11-11 22:15:43 +0700

    fix: production not send back session cookie

--- backend/src/config/init.session.js
+++ backend/src/config/init.session.js
@@ -4,7 +4,7 @@ const sessionConfig = {
   secret: SESSION_SECRET,
   cookie: {
     // enable cookies over https
-    secure: NODE_ENV === "production",
+    secure: false,
 
     // enable to save session to Store
     resave: false,
