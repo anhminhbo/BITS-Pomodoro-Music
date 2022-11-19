0ed827c81a34f1dbad25754fa1fa0210c22e5ad9
Doanh Tran <s3927023@rmit.edu.vn>
2022-11-19 09:40:22 +0700

    img update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
 import { useState, useEffect } from "react";
+import React from "react";
+//import { Route, Link } from "react-route-dom"
+
 import axios from "axios";
 import Timer from "./components/Timer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [backend, setBackend] = useState("No Backend connection");
-  const backendURL = `${window.__RUNTIME_CONFIG__.BACKEND_URL}/api/test`;
-  console.log(backendURL);
-  useEffect(() => {
-    axios.get(backendURL).then((response) => {
-      console.log(response.data.data);
-      setBackend(
-        response.data.data === "Success"
-          ? "Connected to Backend Successfully with Redis"
-          : "Unable to connect to Backend"
-      );
-    });
-  }, []);
+  // const [backend, setBackend] = useState("No Backend connection");
+  // const backendURL = `${window.__RUNTIME_CONFIG__.BACKEND_URL}/api/test`;
+  // console.log(backendURL);
+  // useEffect(() => {
+  //   axios.get(backendURL).then((response) => {
+  //     console.log(response.data.data);
+  //     setBackend(
+  //       response.data.data === "Success"
+  //         ? "Connected to Backend Successfully with Redis"
+  //         : "Unable to connect to Backend"
+  //     );
+  //   });
+  // });
   return (
     <div className="App">
-      {backend}
-      <Timer min={0} sec={5} />
+      {/* {backend} */}
+      <Header />
+      <Footer/>
+      {/* <Timer min={0} sec={5} /> */}
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeaderFooter.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <p>©2022 BUG FIX SQUAD</p>
+    </div>
+  )
+}
+
+export default Footer
