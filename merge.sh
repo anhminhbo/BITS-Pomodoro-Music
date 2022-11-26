be2ee3d6c058077f7707b68d0c558543efccef06
anhminhbo <wormscott12397@gmail.com>
2022-11-27 02:50:55 +0700

    chore: update script for members to merge

---
+++ merge.sh
@@ -0,0 +1,14 @@
+commitMess=$1
+branchName=$2
+
+git add .
+git commit -m"$commitMess"
+git push
+
+git switch minh-dev
+git fetch -p
+git pull
+
+git switch $branchName
+git merge minh-dev
+git push
