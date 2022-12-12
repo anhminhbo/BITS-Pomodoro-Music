f0b9562c2d866ba829eb126b1b8c7bf22d7499c0
anhminhbo <wormscott12397@gmail.com>
2022-12-12 22:13:30 +0700

    chore: init github action for build and push docker

--- dockerize-backend.sh
+++ dockerize-backend.sh
@@ -5,6 +5,8 @@ ROOT_DIR=$(pwd)
 
 cd $ROOT_DIR/backend
 
-DOCKER_BUILDKIT=1 docker build . -t bits-backend:$backendTag
+DOCKER_BUILDKIT=1 docker build . -t anhminhbo/bits-backend:$backendTag
+
+docker push anhminhbo/bits-backend:$backendTag
 
 cd $ROOT_DIR
