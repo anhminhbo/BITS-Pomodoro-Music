b3268287d50fd1d05193087accb3d895ec40dccb
Doanh Tran <s3927023@rmit.edu.vn>
2022-12-16 12:06:48 +0700

    change background

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -21,53 +21,55 @@ const LogIn = () => {
   };
 
   return (
-    <div className="login-register-container login-container">
-      <div className="form-head login-head">
-        Login
-      </div>
-      <form className="pure-form" method="post">
-        <div className="username">
-          <div className="formlabel" for="username">
-            Username
-          </div>
-          <input
-            className="forminput"
-            required
-            id="username"
-            type="text"
-            name="username"
-            maxlength="150"
-            placeholder="Enter Username"
-            ref={username}
-          />
+    <div className="login-register-background">
+      <div className="login-register-container login-container">
+        <div className="form-head login-head">
+          Login
         </div>
-        <div className="password">
-          <div className="formlabel" for="password">
-            Password
+        <form className="pure-form" method="post">
+          <div className="username">
+            <div className="formlabel" for="username">
+              Username
+            </div>
+            <input
+              className="forminput"
+              required
+              id="username"
+              type="text"
+              name="username"
+              maxlength="150"
+              placeholder="Enter Username"
+              ref={username}
+            />
           </div>
-          <input
-            className="forminput"
-            required
-            id="password"
-            type="password"
-            name="password"
-            maxlength="150"
-            placeholder="Enter Password"
-            ref={password}
-          />
+          <div className="password">
+            <div className="formlabel" for="password">
+              Password
+            </div>
+            <input
+              className="forminput"
+              required
+              id="password"
+              type="password"
+              name="password"
+              maxlength="150"
+              placeholder="Enter Password"
+              ref={password}
+            />
+          </div>
+        </form>
+          <button className="form-btn" onClick={() => {
+            console.log(username.current.value);
+            console.log(password.current.value);
+          }}>
+            Log In
+          </button>
+        <div className="form-foot">
+            Don't have an account?{` `}
+            <strong>
+              <a href=""> Register here!</a>
+            </strong>
         </div>
-      </form>
-        <button className="form-btn" onClick={() => {
-          console.log(username.current.value);
-          console.log(password.current.value);
-        }}>
-          Log In
-        </button>
-      <div className="form-foot">
-          Don't have an account?{` `}
-          <strong>
-            <a href=""> Register here!</a>
-          </strong>
       </div>
     </div>
   );
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -59,36 +59,38 @@ const Register = () => {
   
 
   return (
-    <div className="login-register-container register-container">
-      <div className='form-head'>
-        <div className='form-title-bold'>Get started in minutes</div>
-        <div className="form-title-sm">First, let's create your account.</div>
-      </div>
-      <form className='pure-form'>
-        <div className="username">
-            <div className="formlabel" for="username">Username </div>
-            <input className="forminput" type="text" id="username" placeholder="Enter Username" ref={username}/>
+    <div className="login-register-background">
+      <div className="login-register-container register-container">
+        <div className='form-head'>
+          <div className='form-title-bold'>Get started in minutes</div>
+          <div className="form-title-sm">First, let's create your account.</div>
         </div>
-        <div className="email">
-            <div className="formlabel" for="email">Email </div>
-            <input  type="email" id="email" className="forminput" placeholder="Email" ref={email}/>
+        <form className='pure-form'>
+          <div className="username">
+              <div className="formlabel" for="username">Username </div>
+              <input className="forminput" type="text" id="username" placeholder="Enter Username" ref={username}/>
+          </div>
+          <div className="email">
+              <div className="formlabel" for="email">Email </div>
+              <input  type="email" id="email" className="forminput" placeholder="Email" ref={email}/>
+          </div>
+          <div className="password">
+              <div className="formlabel" for="password">Password </div>
+              <input className="forminput" type="password"  id="password" placeholder="Password" ref={password}/>
+          </div>
+          <div className="confirm-password">
+              <div className="formlabel" for="confirmPassword">Confirm Password </div>
+              <input className="forminput" type="password" id="confirmPassword" placeholder="Confirm Password" ref={confirmPassword}/>
+          </div>
+          <div className='checkbox'>
+            <input type="checkbox" className="formbox" id="checkbox" name='checkbox' ref={checkbox}/>
+            <div id='checkbox-inform' for="checkbox">I agree to the <strong>Terms of Service</strong> and <strong>Privacy Policy</strong>.</div>
+          </div>
+        </form>
+        <button className='form-btn' type="submit" onClick={() => handleSubmit()}>Register</button>
+        <div className='form-foot form-foot-register'>
+          Already have an account?{` `} <strong><a href=''> Log in here!</a></strong>
         </div>
-        <div className="password">
-            <div className="formlabel" for="password">Password </div>
-            <input className="forminput" type="password"  id="password" placeholder="Password" ref={password}/>
-        </div>
-        <div className="confirm-password">
-            <div className="formlabel" for="confirmPassword">Confirm Password </div>
-            <input className="forminput" type="password" id="confirmPassword" placeholder="Confirm Password" ref={confirmPassword}/>
-        </div>
-        <div className='checkbox'>
-          <input type="checkbox" className="formbox" id="checkbox" name='checkbox' ref={checkbox}/>
-          <div id='checkbox-inform' for="checkbox">I agree to the <strong>Terms of Service</strong> and <strong>Privacy Policy</strong>.</div>
-        </div>
-      </form>
-      <button className='form-btn' type="submit" onClick={() => handleSubmit()}>Register</button>
-      <div className='form-foot form-foot-register'>
-        Already have an account?{` `} <strong><a href=''> Log in here!</a></strong>
       </div>
     </div>      
   )
