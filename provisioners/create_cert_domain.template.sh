456ae9b7699e6c3cceacef92e097d6f2e4c38e2a
anhminhbo <wormscott12397@gmail.com>
2022-12-17 03:40:22 +0700

    chore: optimize automation script to build infra

---
+++ provisioners/create_cert_domain.template.sh
@@ -0,0 +1,1 @@
+certbot -n --nginx -d [[DOMAIN]] -d www.[[DOMAIN]] -m wormscott12397@gmail.com --agree-tos --redirect
---
+++ provisioners/destroy_infra.sh
@@ -0,0 +1,3 @@
+terraform init
+terraform destroy
+find . -type f -not \( -name 'ansible_hosts.template' -or -name 'ansible.apply_cert.yaml' -or -name 'ansible.config_vm.yaml.template' -or -name 'build_infra.sh' -or -name 'create_cert_domain.template.sh' -or -name 'default.conf' -or -name 'deploy.sh' -or -name 'destroy_infra.sh' -or -name 'docker-compose.yaml.runtime' -or -name 'main.tf' -or -name 'terraform.tfstate' -or -name 'aws-ec2-private-key.pem' \) -delete
