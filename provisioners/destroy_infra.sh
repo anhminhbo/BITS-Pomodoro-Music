dfcab67e522a9a753311cfa7e505e45b5e2b3b7c
anhminhbo <wormscott12397@gmail.com>
2022-12-19 20:50:48 +0700

    chore: optimize build infra script

--- provisioners/destroy_infra.sh
+++ provisioners/destroy_infra.sh
@@ -1,4 +1,6 @@
+echo "Prepare to destroy everything..."
+sleep 5s
 terraform init
-terraform destroy
+terraform destroy -auto-approve
 find . -type f -not \( -name 'ansible_hosts.template' -or -name 'ansible.apply_cert.yaml' -or -name 'ansible.config_vm.yaml.template' -or -name 'build_infra.sh' -or -name 'create_cert_domain.template.sh' -or -name 'default.conf' -or -name 'deploy.sh' -or -name 'destroy_infra.sh' -or -name 'docker-compose.yaml.runtime' -or -name 'main.tf' -or -name 'terraform.tfstate' -or -name 'aws-ec2-private-key.pem' \) -delete
 exit 1
