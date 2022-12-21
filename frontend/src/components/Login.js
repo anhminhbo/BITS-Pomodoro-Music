4c219cf2ba8b0d54700f59021548448673129987
Nguyễn Nguyên Khương <s3924577@rmit.edu.vn>
2022-12-21 10:08:03 +0700

    fix css

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -25,55 +25,55 @@ const LogIn = () => {
   return (
     <>
       <div className="login-background">
-      </div>
-      <div className="login-register-container login-container">
-        <div className="form-head login-head">
-          Login
-        </div>
-        <form className="pure-form" method="post">
-          <div className="username login-register-input">
-            <div className="formlabel" for="username">
-              Username
-            </div>
-            <input
-              className="forminput"
-              required
-              id="username"
-              type="text"
-              name="username"
-              maxlength="150"
-              placeholder="Enter Username"
-              ref={username}
-            />
-          </div>
-          <div className="password login-register-input">
-            <div className="formlabel" for="password">
-              Password
-            </div>
-            <input
-              className="forminput"
-              required
-              id="password"
-              type="password"
-              name="password"
-              maxlength="150"
-              placeholder="Enter Password"
-              ref={password}
-            />
-          </div>
-        </form>
-          <button className="form-btn" onClick={() => {
-            console.log(username.current.value);
-            console.log(password.current.value);
-            login();
-          }}>
+        <div className="login-register-container login-container">
+          <div className="form-head login-head">
             Login
-          </button>
-        <div className="form-foot">
-            Don't have an account?{` `}
-            <strong>
-              <a href="/register"> Register here!</a>
-            </strong>
+          </div>
+          <form className="pure-form" method="post">
+            <div className="username login-register-input">
+              <div className="formlabel" for="username">
+                Username
+              </div>
+              <input
+                className="forminput"
+                required
+                id="username"
+                type="text"
+                name="username"
+                maxlength="150"
+                placeholder="Enter Username"
+                ref={username}
+              />
+            </div>
+            <div className="password login-register-input">
+              <div className="formlabel" for="password">
+                Password
+              </div>
+              <input
+                className="forminput"
+                required
+                id="password"
+                type="password"
+                name="password"
+                maxlength="150"
+                placeholder="Enter Password"
+                ref={password}
+              />
+            </div>
+          </form>
+            <button className="form-btn" onClick={() => {
+              console.log(username.current.value);
+              console.log(password.current.value);
+              login();
+            }}>
+              Login
+            </button>
+          <div className="form-foot">
+              Don't have an account?{` `}
+              <strong>
+                <a href="/register"> Register here!</a>
+              </strong>
+          </div>
         </div>
       </div>
     </>
