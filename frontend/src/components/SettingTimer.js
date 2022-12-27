1cae2aca3d9dc023d66cf08e96432733e82facce
Nguyễn Nguyên Khương <s3924577@rmit.edu.vn>
2022-12-27 11:03:16 +0700

    add animation to timer

--- frontend/src/components/SettingTimer.js
+++ frontend/src/components/SettingTimer.js
@@ -9,62 +9,69 @@ const formatTime = (num) => {
     return num;
 }
 
+const FULL_DASH_ARRAY = 283;
+
 const SettingTimer = () => {
     const [focusLengthMin, setFocusLengthMin] = useState(0);
-    const [focusLengthSec, setFocusLengthSec] = useState(0);
     const [breakLengthMin, setBreakLengthMin] = useState(0);
-    const [breakLengthSec, setBreakLengthSec] = useState(0);
     const [isFocused, setIsFocused] = useState(true);
     const [noti, setNoti] = useState(false);
     const [temp, setTemp] = useState(noti);
-    var Min = (isFocused ? focusLengthMin : breakLengthMin);
-    var Sec = (isFocused ? focusLengthSec : breakLengthSec);
-    const [TimerMin, setTimerMin] = useState(Min);
-    const [TimerSec, setTimerSec] = useState(Sec);
+    const Min = useRef(isFocused ? focusLengthMin : breakLengthMin);
+    const Sec = useRef(0);
+    const [TimerMin, setTimerMin] = useState(Min.current);
+    const [TimerSec, setTimerSec] = useState(Sec.current);
     const [Action, setAction] = useState("Start");
     const Interval = useRef(0);
+    const totalTime = useRef();
 
     useEffect(() => {
-        Min = (isFocused ? focusLengthMin : breakLengthMin);
-        Sec = (isFocused ? focusLengthSec : breakLengthSec);
-        setTimerMin(Min);
-        setTimerSec(Sec);
-    }, [focusLengthMin, focusLengthSec, breakLengthMin, breakLengthSec, isFocused]);
+        Min.current = (isFocused ? focusLengthMin : breakLengthMin);
+        Sec.current = 0;
+        totalTime.current = Min.current * 60;
+        setTimerMin(Min.current);
+        setTimerSec(Sec.current);
+    }, [focusLengthMin, breakLengthMin, isFocused]);
+
+    const setCircleDashArray = (timeFraction) => {
+        const circleDashArray = `${(timeFraction * FULL_DASH_ARRAY).toFixed(0)} 283`;
+        document.getElementsByClassName("timer-path-remaining")[0].setAttribute("stroke-dasharray", circleDashArray);
+    }
 
     // NOTE: THE TIMER ONLY RUN IF INTERVAL EQUALS ZERO
-
     const startAndStopTimer = () => {
         if (Interval.current == 0) {
             setAction("Stop");
             // Start a Timer
             Interval.current = setInterval(() => {
                 // If time runs out
-                if (Min == 0 && Sec == 0) {
+                if (Min.current == 0 && Sec.current == 0) {
                     clearInterval(Interval.current);
-                    setIsFocused(!isFocused);
                     // Set interval to 1 to prevent pressing button while time runs out
-                    Interval.current = 0;
+                    Interval.current = 1;
                     console.log("Stop");
                     setAction("Start");
                 }
                 // If time doesn't run out but second equals to zero
-                else if (Sec == 0) {
-                    Sec = 59;
-                    Min--; 
+                else if (Sec.current == 0) {
+                    Sec.current = 59;
+                    Min.current--; 
                 }
                 // Go normally by reducing second by one
-                else Sec--;
+                else Sec.current--;
                 // Update value for re-render
-                setTimerMin(Min);
-                setTimerSec(Sec);
+                let timeInSec = Min.current*60 + Sec.current;
+                setCircleDashArray(timeInSec / totalTime.current);
+                setTimerMin(Min.current);
+                setTimerSec(Sec.current);
             }, 1000);
         }
         else {
             // If time doesn't run out, allow restart the timer
-            if (Min != 0 && Sec != 0) {
+            if (Min.current != 0 || Sec.current != 0) {
                 setAction("Start");
-                console.log(Min);
-                console.log(Sec);
+                console.log(Min.current);
+                console.log(Sec.current);
                 console.log("Stop");
                 clearInterval(Interval.current);
                 Interval.current = 0;
@@ -111,18 +118,14 @@ const SettingTimer = () => {
 
     useEffect(() => {
         console.log(focusLengthMin);
-        console.log(focusLengthSec);
         console.log(breakLengthMin);
-        console.log(breakLengthSec);
         console.log(noti);
-    }, [focusLengthMin, breakLengthMin, focusLengthSec, breakLengthSec, noti])
+    }, [focusLengthMin, breakLengthMin, noti])
 
     // Handle save properties in Setting
     const handleSave = () => {
         setFocusLengthMin(document.getElementById('setting-focus-length-min').value);
-        setFocusLengthSec(document.getElementById('setting-focus-length-sec').value);
         setBreakLengthMin(document.getElementById('setting-break-length-min').value);
-        setBreakLengthSec(document.getElementById('setting-break-length-sec').value);
         if (noti !== temp) setNoti(temp);
         handleCloseAndOpen();
     }
@@ -130,108 +133,87 @@ const SettingTimer = () => {
     const handleCancel = () => {
         handleCloseAndOpen();
         document.getElementById('setting-focus-length-min').value = focusLengthMin;
-        document.getElementById('setting-focus-length-sec').value = focusLengthSec;
         document.getElementById('setting-break-length-min').value = breakLengthMin;
-        document.getElementById('setting-break-length-sec').value = breakLengthMin;
         document.getElementById('setting-noti').checked = noti;
     }
     
-    
-    // const NumBox2 = useRef(null);
-    // const NumBox1 = useRef(null);
-    // useEffect(() => {
-    //     var invalidChars = ["+","-","e"];
-    //     NumBox1.current.focus
 
-        
-    //     console.log(NumBox2.current)
-    //     console.log(NumBox1.current)
-
-
-    // },[])
-    
-    
-
-
-
-  return (
-    <div>
-        <div className='timer-base'>
-            <svg className="timer-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <g className="timer-circle">
-                <circle className="timer-path_elapsed" cx="50" cy="50" r="45" />
-                <path
-                id='timer-path_left'
-                strokeDasharray="283 283"
-                className="timer-path_remaining "
-                d='
-                M 50, 50
-                m -45, 0
-                a 45,45 0 1,0 90,0
-                a 45,45 0 1,0 -90,0
-                ' />
-                </g>
-            </svg>
-            <div className='timer-label'>
-                <div id="timer-time">{formatTime(TimerMin)}:{formatTime(TimerSec)}</div>
+    return (
+        <div>
+            <div className='timer-base'>
+                <svg className="timer-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                    <g className="timer-circle">
+                    <circle className="timer-path-elapsed" cx="50" cy="50" r="45" />
+                    <path
+                    id='timer-path-left'
+                    strokeDasharray="283 283"
+                    className="timer-path-remaining"
+                    d='
+                    M 50, 50
+                    m -45, 0
+                    a 45,45 0 1,0 90,0
+                    a 45,45 0 1,0 -90,0
+                    ' />
+                    </g>
+                </svg>
+                <div className='timer-label'>
+                    <div id="timer-time">{formatTime(TimerMin)}:{formatTime(TimerSec)}</div>
+                </div>
+                    <input id="timer-btn" type="button" value={Action} style={{display: "block"}} onClick={startAndStopTimer}/>
             </div>
-                <input id="timer-btn" type="button" value={Action} style={{display: "block"}} onClick={startAndStopTimer}/>
+            <button className='setting-button' onClick={() => handleCloseAndOpen()}>
+                Setting
+            </button>
+            <div id="setting-outer" style={style}>
+                <div className='setting-container'>
+                    <div className="setting-header">
+                        <h1 className='setting-title'>Setting</h1> 
+                        <svg className='bi bi-x setting-close-icon' xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" onClick={() => handleCancel()}>
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                    </div>
+                    <ul>
+                        <li className='setting-li'>
+                            <span className='setting-item-name'  style={{transform: "translateY(5px)"}} >
+                                Focus length
+                            </span>
+                            <hstack>
+                                <input id='setting-focus-length-min' placeholder="Minutes" type="number" maxLength='4'  min="1" max="60" className="setting-numbox" defaultValue={focusLengthMin} />
+                            </hstack>
+                            
+                        </li>
+
+                        <li className='setting-li'>
+                            <span className='setting-item-name' style={{transform: "translateY(5px)"}} >
+                                Break length
+                            </span>
+                            <hstack>
+                                <input id='setting-break-length-min' placeholder="Minutes" type="number" step="1"  min="1" className="setting-numbox" defaultValue={breakLengthMin}/>
+                            </hstack>
+                            
+                        </li>
+
+                        <li className='setting-li'>
+                            <span className='setting-item-name'>
+                                Notifications Sound
+                            </span>
+                            <label className='setting-switch'>
+                                <input id='setting-noti' className='setting-checkbox' type="checkbox" defaultChecked={noti}/>
+                                <span className="setting-slider setting-round" onClick={() => {setTemp(!temp)}}></span>
+                            </label>
+                        </li>
+                    </ul>
+
+                    
+                    <div className='form-head'>
+                        <button className='setting-button' onClick={() => handleSave()}>
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </div>  
         </div>
-        <button className='setting-button' onClick={() => handleCloseAndOpen()}>
-            Setting
-        </button>
-        <div id="setting-outer" style={style}>
-            <div className='setting-container'>
-                <div className="setting-header">
-                    <h1 className='setting-title'>Setting</h1> 
-                    <svg className='bi bi-x setting-close-icon' xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" onClick={() => handleCancel()}>
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                    </svg>
-                </div>
-                <ul>
-                    <li className='setting-li'>
-                        <span className='setting-item-name'  style={{transform: "translateY(5px)"}} >
-                            Focus length
-                        </span>
-                        <hstack>
-                            <input id='setting-focus-length-min' placeholder="Minutes" type="number" maxLength='4'  min="1" max="60" className="setting-numbox" defaultValue={focusLengthMin} />
-                            <input id='setting-focus-length-sec' placeholder="Seconds" type="number" min="0" max="60" className="setting-numbox" defaultValue={focusLengthSec}/>
-                        </hstack>
-                        
-                    </li>
-
-                    <li className='setting-li'>
-                        <span className='setting-item-name' style={{transform: "translateY(5px)"}} >
-                            Break length
-                        </span>
-                        <hstack>
-                            <input id='setting-break-length-min' placeholder="Minutes" type="number" step="1"  min="1" className="setting-numbox" defaultValue={breakLengthMin}/>
-                            <input id='setting-break-length-sec' placeholder="Seconds" type="number" step="1"  min="1" className="setting-numbox" defaultValue={breakLengthSec}/>
-                        </hstack>
-                        
-                    </li>
-
-                    <li className='setting-li'>
-                        <span className='setting-item-name'>
-                            Notifications Sound
-                        </span>
-                        <label className='setting-switch'>
-                            <input id='setting-noti' className='setting-checkbox' type="checkbox" defaultChecked={noti}/>
-                            <span className="setting-slider setting-round" onClick={() => {setTemp(!temp)}}></span>
-                        </label>
-                    </li>
-                </ul>
-
-                
-                <div className='form-head'>
-                    <button className='setting-button' onClick={() => handleSave()}>
-                        Save
-                    </button>
-                </div>
-            </div>
-        </div>  
-    </div>
-  )
+    )
 }
 
 export default SettingTimer
