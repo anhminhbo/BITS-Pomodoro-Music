c425146ad384ee22488eafcb7f08b9846d8b4726
Nguyễn Nguyên Khương <s3924577@rmit.edu.vn>
2022-12-29 14:20:12 +0700

    fix music player (adjust next btn and add pre btn)

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -151,6 +151,11 @@ const MusicPlayer = () => {
         return temp;
     }
 
+    // Play back
+    const playBack = () => {
+        return (curIndex - 1 < 0 ? playlist.length - 1 : curIndex - 1);
+    }
+
     // Play next
     const playNext = () => {
         if (random === 0) return (curIndex + 1 >= playlist.length ? 0 : curIndex + 1);
@@ -224,7 +229,12 @@ const MusicPlayer = () => {
                                 <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z"/>
                             </svg>
                         </div>
-                        <div className="music-player-button" onClick={() => {if (curIndex + 1 >= playlist.length) alert("ERROR: End of playlist!"); else setCurIndex(curIndex + 1)}}>
+                        <div className="music-player-button" onClick={() => {setCurIndex(playBack())}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-skip-backward" viewBox="0 0 16 16" style={{transform: "translateY(2.5px)"}}>
+                                <path d="M.5 3.5A.5.5 0 0 1 1 4v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v2.94l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L8.5 8.752v2.94c0 .653-.713.998-1.233.696L1 8.752V12a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm7 1.133L1.696 8 7.5 11.367V4.633zm7.5 0L9.196 8 15 11.367V4.633z"/>
+                            </svg>
+                        </div>
+                        <div className="music-player-button" onClick={() => {setCurIndex(playNext())}}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-skip-forward" viewBox="0 0 16 16" style={{transform: "translateY(2.5px)"}}>
                                 <path d="M15.5 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V8.752l-6.267 3.636c-.52.302-1.233-.043-1.233-.696v-2.94l-6.267 3.636C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696L7.5 7.248v-2.94c0-.653.713-.998 1.233-.696L15 7.248V4a.5.5 0 0 1 .5-.5zM1 4.633v6.734L6.804 8 1 4.633zm7.5 0v6.734L14.304 8 8.5 4.633z"/>
                             </svg>
