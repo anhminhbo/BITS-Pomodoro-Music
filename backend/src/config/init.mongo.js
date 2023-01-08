4edf822da9b2e864378f5c56dc36ab4b1901a7be
anhminhbo <wormscott12397@gmail.com>
2023-01-08 16:34:48 +0700

    chore: disable mongoose debugging

--- backend/src/config/init.mongo.js
+++ backend/src/config/init.mongo.js
@@ -9,13 +9,13 @@ mongoose
   .then(() => console.log("Connecting to Mongoose successfully"))
   .catch((err) => console.error(`Mongo Error: connect:::`, err));
 
-// all executed methods log output to console
-mongoose.set("debug", NODE_ENV === "development");
+// // all executed methods log output to console
+// mongoose.set("debug", NODE_ENV === "development");
 
-// enabled colors in debug mode
-mongoose.set("debug", { color: NODE_ENV === "development" });
+// // enabled colors in debug mode
+// mongoose.set("debug", { color: NODE_ENV === "development" });
 
-// get mongodb-shell friendly output (ISODate)
-mongoose.set("debug", { shell: NODE_ENV === "development" });
+// // get mongodb-shell friendly output (ISODate)
+// mongoose.set("debug", { shell: NODE_ENV === "development" });
 
 module.exports = mongoose;
